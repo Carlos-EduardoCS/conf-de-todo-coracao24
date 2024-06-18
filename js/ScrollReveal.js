0eb7168bb03f39dd418740fc145adeeb60031e0f
Carlos Eduardo <128542237+Carlos-EduardoCS@users.noreply.github.com>
2024-06-18 15:20:49 -0300

    finished

--- js/ScrollReveal.js
+++ js/ScrollReveal.js
@@ -1,12 +1,115 @@
 // geral
 window.sr = ScrollReveal({ 
     resete: true, 
-    duration: 2500,
-    distance:'60px',
 });
 
 sr.reveal('.LOGO-CONF', {
     delay: 200,
-    origin: 'left',
-    duration: 1000,
+    distance:'150px',
+    origin: 'bottom',
+    duration: 3500,
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
 });
+
+sr.reveal('.logo-jni', {
+    delay: 200,
+    distance:'150px',
+    origin: 'left',
+    duration: 3500,
+});
+
+sr.reveal('.btn-mobile', {
+    delay: 200,
+    distance:'150px',
+    duration: 3500,
+});
+
+sr.reveal('.VIDEO-DAYS', {
+    delay: 200,
+    distance:'90px',
+    origin: 'bottom',
+    duration: 2000,
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+});
+
+sr.reveal('.line-up-ttl', {
+    delay: 200,
+    distance:'90px',
+    origin: 'top',
+    duration: 2000,
+});
+
+sr.reveal('.line-up-txt', {
+    delay: 200,
+    distance:'10px',
+    origin: 'right',
+    duration: 2000,
+});
+
+
+sr.reveal('.pregador', {
+    delay: 200,
+    distance:'90px',
+    origin: 'bottom',
+    duration: 2000,
+});
+
+sr.reveal('.', {
+    delay: 200,
+    distance:'90px',
+    origin: 'bottom',
+    duration: 2000,
+});
+
+sr.reveal('.inscricao-ttl', {
+    delay: 200,
+    distance:'',
+    duration: 2000,
+});
+sr.reveal('.infos-txt', {
+    delay: 300,
+    distance:'90px',
+    origin: 'bottom',
+    duration: 2000,
+});
+
+sr.reveal('.jersey-ttl', {
+    delay: 200,
+    distance:'90px',
+    origin: 'top',
+    duration: 2000,
+});
+
+sr.reveal('.camiseta-mockup', {
+    delay: 200,
+    distance:'90px',
+    origin: 'bottom',
+    duration: 2000,
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+});
+
+sr.reveal('.', {
+    delay: 200,
+    distance:'90px',
+    origin: 'bottom',
+    duration: 2000,
+});
+
+sr.reveal('.', {
+    delay: 200,
+    distance:'90px',
+    origin: 'bottom',
+    duration: 2000,
+});
+
+sr.reveal('.', {
+    delay: 200,
+    distance:'90px',
+    origin: 'bottom',
+    duration: 2000,
+});
+
+
+
+
+
